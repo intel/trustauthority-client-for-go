215010a946b5e67e0e9a9abc49f72195b9142fd1
Rawat, Arvind <arvind.rawat@intel.com>
2023-11-09 14:45:16 +0530

    Updated error message and fixed indentation.

--- go-sgx/collect_evidence.go
+++ go-sgx/collect_evidence.go
@@ -5,31 +5,33 @@
  */
 package sgx
 
-// #cgo CFLAGS: -I/opt/intel/sgxsdk/include -fstack-protector-strong
-// #cgo LDFLAGS: -lsgx_dcap_ql -lsgx_urts -ldl -lpthread -L/usr/lib/x86_64-linux-gnu/
-//
-// #include <stdlib.h>
-// #include "sgx_urts.h"
-// #include "sgx_dcap_ql_wrapper.h"
-// #include "sgx_report.h"
-//
-// typedef sgx_status_t (*report_fx) (sgx_enclave_id_t eid,
-//										uint32_t* retval,
-//										const sgx_target_info_t* p_qe3_target,
-//										uint8_t* nonce,
-//										uint32_t nonce_size,
-//										sgx_report_t* p_report);
-//
-// int get_report(report_fx fx,
-//					sgx_enclave_id_t eid,
-//					uint32_t* retval,
-//					const sgx_target_info_t* p_qe3_target,
-//					uint8_t* nonce,
-//					uint32_t nonce_size,
-//					sgx_report_t* p_report)
-// {
-//		return fx(eid, retval, p_qe3_target, nonce, nonce_size, p_report);
-// }
+/*
+#cgo CFLAGS: -I/opt/intel/sgxsdk/include -fstack-protector-strong
+#cgo LDFLAGS: -lsgx_dcap_ql -lsgx_urts -ldl -lpthread -L/usr/lib/x86_64-linux-gnu/
+
+#include <stdlib.h>
+#include "sgx_urts.h"
+#include "sgx_dcap_ql_wrapper.h"
+#include "sgx_report.h"
+
+typedef sgx_status_t (*report_fx) (sgx_enclave_id_t eid,
+                                    uint32_t* retval,
+                                    const sgx_target_info_t* p_qe3_target,
+                                    uint8_t* nonce,
+                                    uint32_t nonce_size,
+                                    sgx_report_t* p_report);
+
+int get_report(report_fx fx,
+                sgx_enclave_id_t eid,
+                uint32_t* retval,
+                const sgx_target_info_t* p_qe3_target,
+                uint8_t* nonce,
+                uint32_t nonce_size,
+                sgx_report_t* p_report)
+{
+	return fx(eid, retval, p_qe3_target, nonce, nonce_size, p_report);
+}
+*/
 import "C"
 import (
 	"unsafe"
@@ -66,7 +68,7 @@ func (adapter *sgxAdapter) CollectEvidence(nonce []byte) (*connector.Evidence, e
 	}
 
 	if retVal != 0 {
-		return nil, errors.Errorf("Report retval returned %x", status)
+		return nil, errors.Errorf("Enclave Report creation returned error %x", retVal)
 	}
 
 	var quote_size C.uint32_t
