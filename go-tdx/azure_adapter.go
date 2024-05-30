1305212353f80a8931b07cfcf694ac2e414276b5
Rawat, Arvind <arvind.rawat@intel.com>
2024-05-30 16:10:15 +0530

    Add sleep time of 3 sec to reflect user data in runtime data.

--- go-tdx/azure_adapter.go
+++ go-tdx/azure_adapter.go
@@ -18,6 +18,7 @@ import (
 	"net/http"
 	"os/exec"
 	"strings"
+	"time"
 
 	"github.com/intel/trustauthority-client/go-connector"
 	"github.com/pkg/errors"
@@ -151,6 +152,9 @@ func getTDReport(reportData []byte) ([]byte, error) {
 	if err != nil {
 		return nil, err
 	}
+
+	// Adding a sleep time of 3s for the user data to be reflected in 0x1400001 nv index
+	time.Sleep(3 * time.Second)
 
 	tdReport, err := exec.Command("tpm2_nvread", "-C", "o", "0x01400001").Output()
 	if err != nil {
