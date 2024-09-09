8eaa509d3ca5d6d70ae1073022e1a87c8ee54fb0
Thompson, Kent <kent.thompson@intel.com>
2024-09-09 08:44:13 -0700

    Address pull request comments.

--- go-tpm/tpm.go
+++ go-tpm/tpm.go
@@ -26,7 +26,7 @@ type TrustedPlatformModule interface {
 	NVExists(nvHandle int) bool
 
 	// NVDefine creates a new nv index with the specified handle and size.  It returns
-	// an error if the handle is not within the range of valid nv ram or if the index.
+	// an error if the handle is not within the range of valid nv indexes.
 	NVDefine(nvHandle int, len int) error
 
 	// NVDelete deletes the specified nv handle/index.  It returns an error if the
@@ -44,13 +44,13 @@ type TrustedPlatformModule interface {
 
 	// GetQuote returns a TPM quote for the given nonce using the specified AK handle.  Rturns
 	// an error if the akHandle is invalid or does not exist.    If 'selection'
-	// is not provided, then all sha1 and sha256 banks will be included in the quote.
+	// is not provided, then all sha256 banks will be included in the quote.
 	GetQuote(akHandle int, nonce []byte, selection ...PcrSelection) ([]byte, []byte, error)
 
 	// GetPcrs returns the "flattened", concatenated, contiguous PCR measurements
 	// for SHA-256 banks 0-23 (in index order).  This is similar to the pcr values
 	// returned in tpm2_quote when '-F values' options are provided.  If 'selection'
-	// is not provided, then all sha1 and sha256 banks are included in the results.
+	// is not provided, then all sha256 banks are included in the results.
 	GetPcrs(selection ...PcrSelection) ([]byte, error)
 
 	// HandleExists is a utility function that returns true if the handle exists in the TPM.
