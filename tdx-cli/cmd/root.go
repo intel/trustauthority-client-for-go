027828f8976232f82027e6dab5729997f178b4c0
Thompson, Kent <kent.thompson@intel.com>
2024-09-11 09:48:12 -0700

    Remove info/time from logrus messages when used in trustauthority-cli.

--- tdx-cli/cmd/root.go
+++ tdx-cli/cmd/root.go
@@ -7,9 +7,11 @@
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/intel/trustauthority-client/tdx-cli/constants"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 )
 
@@ -20,7 +22,16 @@ var rootCmd = &cobra.Command{
 	Long:  ``,
 }
 
+// simpleFormatter is a logrus formatter that logs message without level, time, etc.
+type simpleFormatter struct{}
+
+func (f *simpleFormatter) Format(entry *logrus.Entry) ([]byte, error) {
+	return []byte(fmt.Sprintf("%s\n", entry.Message)), nil
+}
+
 func init() {
+	logrus.SetFormatter(&simpleFormatter{})
+
 	rootCmd.AddCommand(newEvidenceCommand())
 }
 
